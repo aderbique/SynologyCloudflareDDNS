17b9569a4ee9a03b121e99d747fbcda22e22a8c3
Austin Derbique <austin@derbique.org>
2024-05-22 22:04:04 -0700

    Update all A and AAAA records in zone

--- cloudflareddns.sh
+++ cloudflareddns.sh
@@ -4,83 +4,91 @@ set -e
 ipv4Regex="((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])"
 ipv6Regex="(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
 
-ipv6="true"
-# proxy="true"
-# ask for existing proxy, don't override it <.<
-
 # DSM Config
 username="$1"
 password="$2"
 hostname="$3"
 ipAddr="$4"
 
-# Fetch and filter IPv6, if Synology won't provide it
-if [[ $ipv6 == "true" ]]; then
-  ip6fetch=$(ip -6 addr show eth0 | grep -oP "$ipv6Regex" || true)
-  ip6Addr=$(if [ -z "$ip6fetch" ]; then echo ""; else echo "${ip6fetch:0:$((${#ip6fetch})) - 7}"; fi) # in case of NULL, echo NULL
-  recType6="AAAA"
+# Get Zone ID from hostname
+zoneApi="https://api.cloudflare.com/client/v4/zones?name=${hostname}"
+zoneRes=$(curl -s -X GET "$zoneApi" -H "Authorization: Bearer $password" -H "Content-Type:application/json")
+zoneId=$(echo "$zoneRes" | jq -r ".result[0].id")
 
-  if [[ -z "$ip6Addr" ]]; then
-    ipv6="false" # if only ipv4 is available
-  fi
-  if [[ $ipAddr =~ $ipv4Regex ]]; then
-    recordType="A"
-  else
-    recordType="AAAA"
-    ipv6="false" # because Synology had provided the IPv6
-  fi
-else
-  recordType="A"
+if [[ $zoneId == "null" ]]; then
+    echo "badauth"
+    exit 1
 fi
 
-# Cloudflare API-Calls for listing entries
-listDnsApi="https://api.cloudflare.com/client/v4/zones/${username}/dns_records?type=A&name=${hostname}"
-listDnsv6Api="https://api.cloudflare.com/client/v4/zones/${username}/dns_records?type=AAAA&name=${hostname}" # if only IPv4 is provided
+# Cloudflare API-Call for listing all A and AAAA records in the zone
+listARecordsApi="https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?type=A"
+listAAAARecordsApi="https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?type=AAAA"
 
-res=$(curl -s -X GET "$listDnsApi" -H "Authorization: Bearer $password" -H "Content-Type:application/json")
-resSuccess=$(echo "$res" | jq -r ".success")
+# Fetch A records
+resA=$(curl -s -X GET "$listARecordsApi" -H "Authorization: Bearer $password" -H "Content-Type:application/json")
+resASuccess=$(echo "$resA" | jq -r ".success")
 
-if [[ $ipv6 == "true" ]]; then
-  resv6=$(curl -s -X GET "$listDnsv6Api" -H "Authorization: Bearer $password" -H "Content-Type:application/json")
+if [[ $resASuccess != "true" ]]; then
+    echo "badauth"
+    exit 1
 fi
 
-if [[ $resSuccess != "true" ]]; then
-  echo "badauth"
-  exit 1
+# Fetch AAAA records
+resAAAA=$(curl -s -X GET "$listAAAARecordsApi" -H "Authorization: Bearer $password" -H "Content-Type:application/json")
+resAAAASuccess=$(echo "$resAAAA" | jq -r ".success")
+
+if [[ $resAAAASuccess != "true" ]]; then
+    echo "badauth"
+    exit 1
 fi
 
-# Update all IPv4 records
-for record in $(echo "$res" | jq -c '.result[]'); do
-  recordId=$(echo "$record" | jq -r ".id")
-  recordIp=$(echo "$record" | jq -r ".content")
-  recordProx=$(echo "$record" | jq -r ".proxied")
+# Update all A records with the new IP address
+for record in $(echo "$resA" | jq -c '.result[]'); do
+    recordId=$(echo "$record" | jq -r ".id")
+    recordType=$(echo "$record" | jq -r ".type")
+    recordName=$(echo "$record" | jq -r ".name")
+    recordContent=$(echo "$record" | jq -r ".content")
+    recordProxied=$(echo "$record" | jq -r ".proxied")
 
-  if [[ $recordIp != "$ipAddr" ]]; then
-    res=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${username}/dns_records/${recordId}" -H "Authorization: Bearer $password" -H "Content-Type:application/json" --data "{\"type\":\"A\",\"name\":\"$hostname\",\"content\":\"$ipAddr\",\"proxied\":$recordProx}")
-    resSuccess=$(echo "$res" | jq -r ".success")
-    if [[ $resSuccess != "true" ]]; then
-      echo "badauth"
-      exit 1
+    if [[ $recordType == "A" && $recordContent != "$ipAddr" ]]; then
+        updateDnsApi="https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${recordId}"
+        res=$(curl -s -X PUT "$updateDnsApi" -H "Authorization: Bearer $password" -H "Content-Type:application/json" --data "{\"type\":\"A\",\"name\":\"$recordName\",\"content\":\"$ipAddr\",\"proxied\":$recordProxied}")
+        resSuccess=$(echo "$res" | jq -r ".success")
+        if [[ $resSuccess != "true" ]]; then
+            echo "badauth"
+            exit 1
+        fi
     fi
-  fi
 done
 
-# Update all IPv6 records if available
-if [[ $ipv6 == "true" ]]; then
-  for record in $(echo "$resv6" | jq -c '.result[]'); do
-    recordIdv6=$(echo "$record" | jq -r ".id")
-    recordIpv6=$(echo "$record" | jq -r ".content")
-    recordProxv6=$(echo "$record" | jq -r ".proxied")
-
-    if [[ $recordIpv6 != "$ip6Addr" ]]; then
-      res6=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${username}/dns_records/${recordIdv6}" -H "Authorization: Bearer $password" -H "Content-Type:application/json" --data "{\"type\":\"AAAA\",\"name\":\"$hostname\",\"content\":\"$ip6Addr\",\"proxied\":$recordProxv6}")
-      res6Success=$(echo "$res6" | jq -r ".success")
-      if [[ $res6Success != "true" ]]; then
-        echo "badauth"
-        exit 1
-      fi
+# Fetch the IPv6 address if required
+if [[ $ipv6 = "true" ]]; then
+    ip6fetch=$(ip -6 addr show eth0 | grep -oP "$ipv6Regex" || true)
+    ip6Addr=$(if [ -z "$ip6fetch" ]; then echo ""; else echo "$ip6fetch"; fi)
+    if [[ -z "$ip6Addr" ]]; then
+        ipv6="false"
     fi
-  done
+fi
+
+# Update all AAAA records with the new IPv6 address
+if [[ $ipv6 = "true" && ! -z "$ip6Addr" ]]; then
+    for record in $(echo "$resAAAA" | jq -c '.result[]'); do
+        recordId=$(echo "$record" | jq -r ".id")
+        recordType=$(echo "$record" | jq -r ".type")
+        recordName=$(echo "$record" | jq -r ".name")
+        recordContent=$(echo "$record" | jq -r ".content")
+        recordProxied=$(echo "$record" | jq -r ".proxied")
+
+        if [[ $recordType == "AAAA" && $recordContent != "$ip6Addr" ]]; then
+            updateDnsApi="https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${recordId}"
+            res=$(curl -s -X PUT "$updateDnsApi" -H "Authorization: Bearer $password" -H "Content-Type:application/json" --data "{\"type\":\"AAAA\",\"name\":\"$recordName\",\"content\":\"$ip6Addr\",\"proxied\":$recordProxied}")
+            resSuccess=$(echo "$res" | jq -r ".success")
+            if [[ $resSuccess != "true" ]]; then
+                echo "badauth"
+                exit 1
+            fi
+        fi
+    done
 fi
 
 echo "good"
